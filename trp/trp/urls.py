0733b4b7b104195046fae4bda146e3ade6e8e060
vineetkashyap <vkvineet66@gmail.com>
2021-05-08 15:39:09 +0530

    add

--- trp/trp/urls.py
+++ trp/trp/urls.py
@@ -7,15 +7,14 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 from rest_framework.routers import DefaultRouter
 router= DefaultRouter()
-# router.register('reg1',views.Transporter_View,basename='reg1')
-# ajdhfjasdhfja
+router.register('reg1',views.Transporter_View,basename='reg1')
 # router.register('reg2',views.Truck_Owner_View,basename='reg2')
 # router.register('reg3',views.Vehicle_Registration_View,basename='reg3')
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('',include(router.urls)),
-    # path('auth/',include('rest_framework.urls')),
-    # path('gettoken/',obtain_auth_token),
+    path('',include(router.urls)),
+    path('auth/',include('rest_framework.urls')),
+    path('gettoken/',obtain_auth_token),
     path('stu/',views.student_data,name='stu'),
 ]+static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
 
