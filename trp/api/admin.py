c9fb9a01657c5dedf5b580432e9b565dfd592595
vineetkashyap <vkvineet66@gmail.com>
2021-05-08 18:05:31 +0530

    origin

--- trp/api/admin.py
+++ trp/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+@admin.register(TruckOwnerModel)
+class TruckOwnerModelAdmin(admin.ModelAdmin):
+    cancelled_cheque_xerox = models.FileField(upload_to='media', max_length=100)
+    sign = models.FileField(upload_to='media', max_length=100)
+    list_display = ['name','father_name','mobile_number','alternate_mobile_number','email_id','address','aadhar_number','pan_number','bank_account_name','bank_account_number','ifsc_code','bank_name','branch_name','aadhar_xerox','pan_card_xerox','cancelled_cheque_xerox','photo','photo','sign']
