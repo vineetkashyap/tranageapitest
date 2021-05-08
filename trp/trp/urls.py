81fdde3a9dc0e886fd7f3dff2ee525e1f4db5d22
vineetkashyap <vkvineet66@gmail.com>
2021-05-08 15:25:05 +0530

    commnet

--- trp/trp/urls.py
+++ trp/trp/urls.py
@@ -7,7 +7,8 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 from rest_framework.routers import DefaultRouter
 router= DefaultRouter()
-router.register('reg1',views.Transporter_View,basename='reg1')
+# router.register('reg1',views.Transporter_View,basename='reg1')
+# ajdhfjasdhfja
 # router.register('reg2',views.Truck_Owner_View,basename='reg2')
 # router.register('reg3',views.Vehicle_Registration_View,basename='reg3')
 urlpatterns = [
